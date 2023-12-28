79d4a798297d99f070827da00e22fc17760aff6c
Kota Yamamoto <s22c1133uz@s.chibakoudai.jp>
2023-12-28 12:05:30 +0900

    Add a command

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,32 +1,12 @@
-import rclpy                     #ROS 2のクライアントのためのライブラリ
-from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
-from person_msgs.srv import Query   #通信の型（16ビットの符号付き整数）
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
 
-def main():
-    rclpy.init()
-    node = Node("listener")            #ノード作成（nodeという「オブジェクト」を作成）
-    client = node.create_client(Query, 'query')   #パブリッシャのオブジェクト作成
-    while not client.wait_for_service(timeout_sec=1.0):
-        node.get_logger().info('待機中')
+def cb(msg):
+    global node
+    node.get_logger().info("Listen: %d" % msg.data)
 
-    req = Query.Request()
-    req.name = "山元広太"
-    future = client.call_async(req)
-
-    while rclpy.ok():
-        rclpy.spin_once(node)
-        if future.done():
-            try:
-                response = future.result()
-            except:
-                node.get_logger().info('呼び出し失敗')
-            else:
-                node.get_logger().info("age: {}".format(response.age))
-
-            break
-
-    node.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
+rclpy.init()
+node = Node("listener")
+pub = node.create_subscription(Int16, "countup", cb, 10)
+rclpy.spin(node)
