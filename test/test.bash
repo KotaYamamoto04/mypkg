85c599e86add232c202a0c5086a584e655556c6a
Kota Yamamoto <s22c1133uz@s.chibakoudai.jp>
2023-12-30 17:40:00 +0900

    A

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2023 Kota Yamamoto s22c1133uz@s.chibakoudai.jp
+#SPDX-Lincense-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
