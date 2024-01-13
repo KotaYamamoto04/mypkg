fbd8022810688c454dbaac7ae92216be5f3e4aca
Kota Yamamoto <s22c1133uz@s.chibakoudai.jp>
2024-01-13 14:54:11 +0900

    add a yml

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
- #!/bin/bash
+#!/bin/bash
+#SPDX-FileCopyrightText: 2023 Kota Yamamoto s22c1133uz@s.chibakoudai.jp
+#SPDX-License-Identifier:BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
