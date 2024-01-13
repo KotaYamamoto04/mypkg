59b22577d7c368040c84b640bdb2635fe0a0b447
Kota Yamamoto <s22c1133uz@s.chibakoudai.jp>
2024-01-13 13:33:49 +0900

    A

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,40 +1,19 @@
 #SPDX-FileCopyrightText: 2023 Kota Yamamoto s22c1133uz@s.chibakoudai.jp
 #SPDX-License-Identifier:BSD-3-Clause
-
-import rclpy                     #ROS 2のクライアントのためのライブラリ
-from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
-from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
-
-class Talker():          #ヘッダの下にTalkerというクラスを作成
-    def __init__(self, nh):  # オブジェクトを作ると呼ばれる関数
-        self.pub = node.create_publisher(Int16, "countup", 10)
-        self.n = 0
-        nh.create_timer(0.5, self.cb)
-         # ↑ selfはオブジェクトのこと
-         # ↑ オブジェクトにひとつパブリッシャと変数をもたせる。
-    def cb(self):              
-        msg = Int16()
-        msg.data = self.n
-        self.pub.publish(msg)
-        self.n += 1
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
 
 rclpy.init()
-<<<<<<< HEAD
-node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-pub = node.create_publisher(Int16, "countup", 10)   #パブリッシャのオブジェクト作成
-n = 0 #カウント用変数
-
-def cb():          #17行目で定期実行されるコールバック関数
-    global n       #関数を抜けてもnがリセットされないようにしている
-    msg = Int16()  #メッセージの「オブジェクト」
-    msg.data = n   #msgオブジェクトの持つdataにnを結び付け
-    pub.publish(msg)        #pubの持つpublishでメッセージ送信    
+node = Node("talker")
+pub = node.create_publisher(Int16, "countup", 10)
+n = 0
+def cb():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
     n += 1
 
-node.create_timer(0.5, cb)  #タイマー設定
-rclpy.spin(node) #実行（無限ループ）
-=======
-node = Node("talker")
-talker = Talker(node)
+node.create_timer(0.5, cb)
 rclpy.spin(node)
->>>>>>> lesson11
